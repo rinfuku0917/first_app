68ec90c05851d7401e7dba5397d2a44b66999039
rinfuku0917 <fuku@h-noname.com>
2021-06-21 16:18:20 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+    def index
+      @posts = Post.all # すべてのレコードを@postsに代入
+    end
+
+    def create
+      Post.create(content: params[:content])
+    end
+end
